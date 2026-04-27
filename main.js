0d776892e4b8d63f770960a97c97e4bdef14e0bf
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-04-27 10:16:04 -0300

    0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll(".botao");
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
-        for(let j=o;j<botoes.length;j++){
+        for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo")
         }
 
