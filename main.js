881ca6310e7c2086e87e6e153e9f05c1ccdde5cf
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-05-25 08:01:26 -0300

    mudar

--- main.js
+++ main.js
@@ -35,17 +35,21 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 
 }
 
 
 function atualizaCronometro() {
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
     for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        //contadores[i].textContent = calculaTempo(tempos[i]);
 
     }
 }
@@ -54,4 +58,4 @@ function comecaCronometro() {
     setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
